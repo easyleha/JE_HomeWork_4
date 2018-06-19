019f5bbbd1e3631c324191ea91e26649307d0ab0
Marina <marinash.rush@gmail.com>
2018-06-19 17:31:19 +0300

    доделал

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,37 @@
 package com.company;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
     private static Name name;
     private static Surname surname;
     private static int age;
     private static Group group;
+    private static List<User> studentList = new ArrayList<>();
 
     public static void main(String[] args) {
 
         CreateUser createUser = new CreateUser();
         createUser.createUserList();
+        createStudentList();
 
-        Student student = new Student(name, surname, age);
-        student.createStudentList();
 
+
+
+    }
+    public static void createStudentList() {
+        Random random = new Random();
+        for (int i = 0; i <= 100; i++) {
+            studentList.add(new Student(Name.values()[random.nextInt(Name.values().length)],
+                    Surname.values()[random.nextInt(Surname.values().length)], random.nextInt(20) + 20));
+        }
+        Collections.sort(studentList);
+        System.out.println("studentList: " + studentList);
 
     }
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -9,7 +9,6 @@ public class Student extends User {
 
 
 
-    List<User> studentList = new ArrayList<>();
     public Group group;
     private static Random random = new Random();
 
@@ -20,16 +19,6 @@ public class Student extends User {
         this.group = Group.values()[random.nextInt(Group.values().length)];
     }
 
-    public void createStudentList() {
-        Random random = new Random();
-        for (int i = 0; i <= 100; i++) {
-            studentList.add(new Student(Name.values()[random.nextInt(Name.values().length)],
-                    Surname.values()[random.nextInt(Surname.values().length)], random.nextInt(20) + 20));
-        }
-        Collections.sort(studentList);
-        System.out.println("studentList: " + studentList);
-
-    }
 
     @Override
     public int compareTo(User user) {
